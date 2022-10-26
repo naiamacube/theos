cf391af08fde7956b81d06d2e2c5537e28ebce84
alxyok <alxyok@naiama.com>
2022-10-26 14:43:01 +0000

    adjust read and write throughput for DynamoDB

--- dokimi.tf
+++ dokimi.tf
@@ -9,6 +9,8 @@ resource "aws_s3_bucket_versioning" "backend-storage" {
 resource "aws_dynamodb_table" "backend-locking" {
   name = "n3-dokimi-tfstate"
   hash_key = "LockID"
+  read_capacity = 1
+  write_capacity = 1
 
   attribute {
     name = "LockID"
