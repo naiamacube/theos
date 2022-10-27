ad7688f12376d44f0188c40072a41c788632c51c
alxyok <alxyok@naiama.com>
2022-10-27 11:03:42 +0000

    fix variables naming issue

--- main.tf
+++ main.tf
@@ -22,8 +22,8 @@ terraform {
 }
 
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
 
 provider "google" {
