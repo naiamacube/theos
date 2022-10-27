686a2c32e7b3b28583d5480784fd0cd2755031a5
alxyok <alxyok@naiama.com>
2022-10-27 11:01:47 +0000

    removed unused variables

--- main.tf
+++ main.tf
@@ -24,12 +24,10 @@ terraform {
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region = var.aws_region
 }
 
 provider "google" {
   credentials = var.gcp_credentials
-  region = var.gcp_region
 }
 
 module "dokimi" {
