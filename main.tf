8d98e0563860cccefd76ffd4048bc86ccaf787df
alxyok <alxyok@naiama.com>
2022-10-28 11:48:19 +0000

    remove unnecessary variables

--- main.tf
+++ main.tf
@@ -41,10 +41,6 @@ module "dokimi" {
 
   aws_access_key  = var.aws_access_key_id
   aws_secret_key  = var.aws_secret_access_key
-  aws_region      = var.dokimi_aws_region
-  aws_backend     = var.dokimi_aws_backend
-  aws_iam         = var.dokimi_aws_iam
 
   gcp_credentials = var.gcp_credentials
-  gcp_region      = var.dokimi_gcp_region
 }
--- modules/dokimi/main.tf
+++ modules/dokimi/main.tf
@@ -21,10 +21,12 @@ terraform {
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
+
   region     = local.aws_region
 }
 
 provider "google" {
   credentials = var.gcp_credentials
+
   region      = local.gcp_region
 }
